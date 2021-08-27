79a06f4d1f6b8a5a0149d211fe0fb53e33b57a1d
Horacio Crespo <horacio.crpo@gmail.com>
2021-08-27 15:15:50 -0400

    Normalize string before comparing

--- src/main/java/com/jobsity/tenpinbowlingscore/utils/FileUtils.java
+++ src/main/java/com/jobsity/tenpinbowlingscore/utils/FileUtils.java
@@ -3,13 +3,18 @@ package com.jobsity.tenpinbowlingscore.utils;
 public class FileUtils {
 
     private static final String VALID_LINE_REGEX = "[a-zA-Z]+\\t\\b([0-9]|10|F)\\b";
-    public static final String CRLF = "\r\n";
+    public static final String LF = "\n";
+    public static final String CRLF_REGEX = "\\r\\n?";
 
     public static boolean isLineValid(String line) {
         if (line == null) {
             return false;
         }
         return line.matches(VALID_LINE_REGEX);
+    }
+
+    public static String replaceCRLFWithLF(String text) {
+        return text.replaceAll(CRLF_REGEX, LF);
     }
 
 }
--- src/test/java/com/jobsity/tenpinbowlingscore/integration/ScoreReaderIntegrationTest.java
+++ src/test/java/com/jobsity/tenpinbowlingscore/integration/ScoreReaderIntegrationTest.java
@@ -24,33 +24,33 @@ public class ScoreReaderIntegrationTest {
 
     @Test
     public void canHandlePerfectScoreWithOnePlayer() throws IOException {
-        String expected = "Frame\t\t1\t\t2\t\t3\t\t4\t\t5\t\t6\t\t7\t\t8\t\t9\t\t10" + FileUtils.CRLF +
-                "Carl" + FileUtils.CRLF +
-                "PinFalls\t\tX\t\tX\t\tX\t\tX\t\tX\t\tX\t\tX\t\tX\t\tX\tX\tX\tX" + FileUtils.CRLF +
-                "Score\t\t30\t\t60\t\t90\t\t120\t\t150\t\t180\t\t210\t\t240\t\t270\t\t300" + FileUtils.CRLF;
+        String expected = "Frame\t\t1\t\t2\t\t3\t\t4\t\t5\t\t6\t\t7\t\t8\t\t9\t\t10" + FileUtils.LF +
+                "Carl" + FileUtils.LF +
+                "PinFalls\t\tX\t\tX\t\tX\t\tX\t\tX\t\tX\t\tX\t\tX\t\tX\tX\tX\tX" + FileUtils.LF +
+                "Score\t\t30\t\t60\t\t90\t\t120\t\t150\t\t180\t\t210\t\t240\t\t270\t\t300" + FileUtils.LF;
         StringWriter stringWriter = new StringWriter();
         scoreReader.read(
                 new BufferedReader(new StringReader("src/test/resources/positive/perfect.txt")),
                 new BufferedWriter(stringWriter));
 
-        Assert.assertEquals(expected, stringWriter.toString());
+        Assert.assertEquals(expected, FileUtils.replaceCRLFWithLF(stringWriter.toString()));
     }
 
     @Test
     public void canHandleAMatchBetweenTwoPlayers() throws IOException {
-        String expected = "Frame\t\t1\t\t2\t\t3\t\t4\t\t5\t\t6\t\t7\t\t8\t\t9\t\t10" + FileUtils.CRLF +
-                "Jeff" + FileUtils.CRLF +
-                "PinFalls\t\tX\t7\t/\t9\t0\t\tX\t0\t8\t8\t/\t0\t6\t\tX\t\tX\tX\t8\t1" + FileUtils.CRLF +
-                "Score\t\t20\t\t39\t\t48\t\t66\t\t74\t\t84\t\t90\t\t120\t\t148\t\t167" + FileUtils.CRLF +
-                "John" + FileUtils.CRLF +
-                "PinFalls\t3\t/\t6\t3\t\tX\t8\t1\t\tX\t\tX\t9\t0\t7\t/\t4\t4\tX\t9\t0" + FileUtils.CRLF +
-                "Score\t\t16\t\t25\t\t44\t\t53\t\t82\t\t101\t\t110\t\t124\t\t132\t\t151" + FileUtils.CRLF;
+        String expected = "Frame\t\t1\t\t2\t\t3\t\t4\t\t5\t\t6\t\t7\t\t8\t\t9\t\t10" + FileUtils.LF +
+                "Jeff" + FileUtils.LF +
+                "PinFalls\t\tX\t7\t/\t9\t0\t\tX\t0\t8\t8\t/\t0\t6\t\tX\t\tX\tX\t8\t1" + FileUtils.LF +
+                "Score\t\t20\t\t39\t\t48\t\t66\t\t74\t\t84\t\t90\t\t120\t\t148\t\t167" + FileUtils.LF +
+                "John" + FileUtils.LF +
+                "PinFalls\t3\t/\t6\t3\t\tX\t8\t1\t\tX\t\tX\t9\t0\t7\t/\t4\t4\tX\t9\t0" + FileUtils.LF +
+                "Score\t\t16\t\t25\t\t44\t\t53\t\t82\t\t101\t\t110\t\t124\t\t132\t\t151" + FileUtils.LF;
         StringWriter stringWriter = new StringWriter();
         scoreReader.read(
                 new BufferedReader(new StringReader("src/test/resources/positive/scores.txt")),
                 new BufferedWriter(stringWriter));
 
-        Assert.assertEquals(expected, stringWriter.toString());
+        Assert.assertEquals(expected, FileUtils.replaceCRLFWithLF(stringWriter.toString()));
     }
 
     /* Negative scenarios */
